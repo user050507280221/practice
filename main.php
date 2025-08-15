82863b9acde6e810c5975252a65d7d195addfda8
user050507280221 <angelbenitezayado477@gmail.com>
2025-08-15 06:47:31 +0000

    git c

--- main.php
+++ main.php
@@ -2,11 +2,13 @@
 $name = "Elijah Quiniquito";
 $age = "14";
 $address = "Heroes del 76 Brgy. 71 Caloocan City";
-$gender = "yes";
+$gender = "yes <br>";
+$crush = "yes";
 
 echo " I am $name. <br>";
 echo "I am $age years old. <br>";
 echo "I live at $address. <br>";
 echo "Are you a boy?: $gender";
+echo "Do you have a crush? $crush"
 
 ?>
